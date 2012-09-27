dc674cd3a2aeeeb4937e353befc35120e47b4e88
topa <paul.torka@yahoo.de>
2012-09-27 10:15:07 +0200

    - removed second page. all the stuff is now handled by the main page.

--- app/pages/MainPage.class.js
+++ app/pages/MainPage.class.js
@@ -3,9 +3,27 @@
 var alamid = require("alamid"),
     Page = alamid.Page;
 
+var TodoListWrapperView = require("../views/todolist/wrapper/TodoListWrapperView.class.js");
+
 var MainPage = Page.define("MainPage", {
 
-    $template: require("./MainPage.html")
+    /**
+     * TodoListWrapperView
+     */
+    __wrapper: null,
+
+    __footer: null,
+
+    $template: require("./MainPage.html"),
+
+    init: function () {
+
+        this.Super();
+
+        this.__wrapper = new TodoListWrapperView();
+        this.Super._prepend(this.__wrapper).at("page");
+
+    }
 
 });
 
