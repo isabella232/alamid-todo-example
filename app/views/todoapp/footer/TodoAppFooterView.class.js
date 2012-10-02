3b99d1a2dc6773e7148d74ef1c0fde3182a04ef0
topa <paul.torka@yahoo.de>
2012-10-02 10:31:18 +0200

    - provides now a function for setting todo count - proxies now clicks on filters

--- app/views/todoapp/footer/TodoAppFooterView.class.js
+++ app/views/todoapp/footer/TodoAppFooterView.class.js
@@ -5,7 +5,51 @@ var alamid = require("alamid"),
 
 var TodoAppFooterView = View.define("TodoAppFooterView", {
 
-    $template: require("./TodoAppFooterView.html")
+    $template: require("./TodoAppFooterView.html"),
+
+    init: function () {
+
+        this.Super();
+
+        this._initNodeEvents();
+    },
+
+    /**
+     * @param {Number} count
+     */
+    setTodoCount: function (count) {
+        var nodeMap = this.Super._getNodeMap();
+        nodeMap["todo-count"].innerText = count;
+    },
+
+    _initNodeEvents: function () {
+
+        var self = this;
+
+        this.Super._addNodeEvents({
+            "all": {
+                "click": function () {
+                    self.Super.emit("showAll");
+                }
+            },
+            "active": {
+                "click": function () {
+                    self.Super.emit("showActive");
+                }
+            },
+            "completed": {
+                "click": function () {
+                    self.Super.emit("showCompleted");
+                }
+            },
+            "clear-completed": {
+                "click": function () {
+                    self.Super.emit("clearCompleted");
+                }
+            }
+        });
+
+    }
 
 });
 
