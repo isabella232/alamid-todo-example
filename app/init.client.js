5c183f153ccb41d2c6b023a34e982bbf25e9b659
meaku <michael.jaser@roomieplanet.de>
2012-12-14 17:04:29 +0100

    - Todo-Model can now be used from console because it's attached to window

--- app/init.client.js
+++ app/init.client.js
@@ -4,14 +4,19 @@ var alamid = require("alamid"),
     app = alamid.app,
     jQuery = alamid.util.jQuery;
 
+//need for attaching, demo purposes
+var Todo = require("./models/todo/TodoModel.class.js");
+
 app.addRoute("*", function () {
     // Overwrite alamid's default 404 Page and do nothing,
     // Because in this app there is only one Page: MainPage.
 });
 
-window._ = require("alamid").util.underscore;
-
 jQuery(document).ready(function onDomReady() {
+
+    //attach references of the TodoModel to the window objects, so you can play with it
+    window.Todo = Todo;
+
     app.start();
     //to be done after websockets have been enabled via app.start()
     app.enableModelSubscribe();
